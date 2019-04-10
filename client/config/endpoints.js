7a248eea342189dd05b70174ffc2535f7e88eb00
Bijay Timilsina <bjtimi.007@gmail.com>
2019-04-10 11:07:02 +0200

    Add apollo client with basic configs

---
+++ client/config/endpoints.js
@@ -0,0 +1,12 @@
+const url = {
+  ip: {
+    local: '0.0.0.0',
+    http: 'localhost'
+  },
+  port: {
+    local: '4000',
+    http: '4000'
+  }
+}
+
+export default url;
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import apolloClientOptions from '../config/apolloConfig';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>Time to show em</div>
-    )
-  }
+export default () => {
+  const apolloClient = new ApolloClient({...apolloClientOptions});
+  // Apollo provider as top level wrapper that can manage state and query manipulations in apollo cache for our whole app
+  return (
+    <ApolloProvider client={apolloClient}>
+      <div>Check react dev tools to see what the client object provides ;)</div>
+    </ApolloProvider>
+  )
 }
