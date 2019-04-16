edadac6a3c6cfdb7c4039a7db32a00bf1ab65cd2
Bijay Timilsina <bjtimi.007@gmail.com>
2019-04-16 12:56:44 +0200

    Replace react-native stuffs  with cra injected stuffs

--- chat_client/src/App.js
+++ chat_client/src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import apolloClientOptions from '../config/apolloConfig';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import MainNavigator from './navigation/index';
-
-export default () => {
-  const apolloClient = new ApolloClient({...apolloClientOptions});
-  // Apollo provider as top level wrapper to manage local state and query manipulations
-  return (
-    <ApolloProvider client={apolloClient}>
-      <MainNavigator />
-    </ApolloProvider>
-  )
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+export default App;
